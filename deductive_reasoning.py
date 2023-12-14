50270765df83d3b7637c7c961da36cf678b43a42
Nianqi-Li <141998560+Nianqi-Li@users.noreply.github.com>
2023-12-14 15:11:36 +0800

    Update deductive_reasoning.py

--- deductive_reasoning.py
+++ deductive_reasoning.py
@@ -1,47 +1,12 @@
 import json
-import random
 from bs4 import BeautifulSoup
-import openai
 from tqdm import tqdm
 import numpy as np
 import requests
 import wikipedia
+from utils import llm
 
-openai.api_key = 'sk-*'
-EMBEDDING_MODEL = "text-embedding-ada-002"
-GENERATING_MODEL = "gpt-3.5-turbo"
 soup = BeautifulSoup(features="html.parser")
-
-def llm(model_type,instruction,examples,question,model=None,tokenizer=None,stop_words=['\n']):
-    if model_type == 'chatgpt':
-        return chatgpt(instruction,examples,question,stop_words)
-    elif model_type == 'llama':
-        return llama(instruction,examples,question,model,tokenizer,stop_words)
-
-def chatgpt(instruction,examples,question, stop=["\n"]):
-    response = openai.ChatCompletion.create(
-      model=GENERATING_MODEL,
-      messages=[{"role": "system", "content": instruction},
-               {"role": "assistant", "content": examples},
-               {"role": "user", "content": question}],
-      temperature=0,
-      max_tokens=100,
-      top_p=1,
-      frequency_penalty=0.0,
-      presence_penalty=0.0,
-      stop=stop
-    )
-    return response["choices"][0]["message"]["content"]
-
-def llama(instruction,examples,question,model,tokenizer,stop_words=['\n']):
-    text = instruction + examples + question
-    text_token = tokenizer(text, return_tensors="pt")
-    output_ids = model.generate(text_token.input_ids.to(model.device), max_new_tokens=256)
-    ans = tokenizer.batch_decode(output_ids, skip_special_tokens=True, clean_up_tokenization_spaces=False)[0]
-    ans = ans[len(text):]
-    if len(stop_words) > 0:
-        ans = ans[:min([ans.find(w) for w in stop_words if ans.find(w)!=-1] + [len(ans)])]
-    return ans
 
 def entity_search(data,mention):
     with open(template_path,'r',encoding='utf-8') as f:
